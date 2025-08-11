d4374184e2b305212293e2631634e41f2ec4894f
blurbler2 <zeka.blerta+github@gmail.com>
2025-08-11 17:27:50 +0200

    fix: typo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 http.createServer(function (req, res) {
-	res.write("ON the way to being a fullstack engineer!");
+	res.write("On the way to being a fullstack engineer!");
 	res.end();
 }).listen(3000);
 
