5b5d4b978825d7a204df693aa9adef94b3ec8161
blurbler2 <zeka.blerta+github@gmail.com>
2025-11-24 15:09:34 +0100

    fix: typo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 http.createServer(function (req, res) {
-	res.write("On the way to being a fullstack engineer! :D\n");
+	res.write("On the way to being a full stack engineer! :D\n");
 	res.end();
 }).listen(3000);
 
-console.log("Server started on port 300");
+console.log("Server started on port 3000");
