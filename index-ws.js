b4a1ea9beb2c76d2fc3a8d4237b9860d6007c922
blurbler2 <zeka.blerta+github@gmail.com>
2025-11-25 13:17:44 +0100

    feat: add sqlite and database logging

--- index-ws.js
+++ index-ws.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const { get } = require('http');
 const server = require('http').createServer();
 const app = express();
 const port = 3000;
@@ -12,6 +13,19 @@ server.on('request', app);
 server.listen(port, () => {
   console.log(`Express server listening at http://localhost:${port}`);
 });	
+
+// interrupt handler to close the database connection gracefully
+process.on('SIGINT', () => {
+    console.log('Received SIGINT. Shutting down gracefully...');
+    // close all websocket connections
+    wss.clients.forEach((client) => {
+        client.close();
+    });
+    console.log('Closed all websocket connections.');
+    server.close(() => {
+        shutdownDatabase(); 
+    });
+});
 
 /** Begin websockets */
 const WebSocketServer = require('ws').Server;
@@ -27,6 +41,14 @@ wss.on('connection', function connection(ws) {
     if(ws.readyState === ws.OPEN){
             ws.send(`Welcome to my server! There are currently ${numClients} visitors online.`);
     }
+
+    // write session info to database
+    db.run(`INSERT INTO visitors(count, time) VALUES(${numClients}, datetime('now'))` , function(err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+    });
 
     ws.on('close', function close() {
         wss.broadcast(`Current visitors online: ${numClients}`);
@@ -45,3 +67,41 @@ wss.broadcast = function broadcast(data) {
 /** End websockets */   
 
 console.log(`WebSocket server started on port ${port}`);
+
+/** Begin database */
+
+const sqlite = require('sqlite3');
+// const db = new sqlite.Database('./database.db');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+    db.each('SELECT * FROM visitors', (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log(`Count: ${row.count}, Time: ${row.time}`);
+    });
+}
+
+function shutdownDatabase() {
+    getCounts();
+    console.log('Shutting down database...');
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Closed the database connection.');
+    });
+
+}
+
+/** End database */
